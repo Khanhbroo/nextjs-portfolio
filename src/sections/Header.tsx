38a5d69687a46ce204041f8287ea36a62846abc9
Khanhbroo <khanhmathias532211@gmail.com>
2025-07-21 05:52:54 +0700

    feat: adding active feature for header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,24 +1,53 @@
-import Link from "next/link";
+"use client";
+
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+
+const navItems = [
+  {
+    href: "#hero",
+    title: "Home",
+    active: true,
+  },
+  {
+    href: "#projects",
+    title: "Projects",
+  },
+  {
+    href: "#about",
+    title: "About",
+  },
+  {
+    href: "#contact",
+    title: "Contact",
+  },
+];
 
 export const Header = () => {
+  const [activeItem, setActiveItem] = useState("Home");
+
+  const handleActive = (title: string) => {
+    setActiveItem(title);
+  };
+
   return (
     <div className="flex justify-center items-center fixed top-3 w-full z-10">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-        <Link href="#hero" className="nav-item">
-          Home
-        </Link>
-        <Link href="#projects" className="nav-item">
-          Projects
-        </Link>
-        <Link href="#about" className="nav-item">
-          About
-        </Link>
-        <Link
-          href="#contact"
-          className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
-        >
-          Contact
-        </Link>
+        {navItems.map((item) => (
+          <a
+            href={item.href}
+            key={item.title}
+            className={twMerge(
+              "nav-item",
+              activeItem === item.title
+                ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
+                : "undefined"
+            )}
+            onClick={() => handleActive(item.title)}
+          >
+            {item.title}
+          </a>
+        ))}
       </nav>
     </div>
   );
